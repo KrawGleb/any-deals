6f77a6efefb32308d8c6bbbd7007b5f79c129896
KrawGleb <krawcevitsch@gmail.com>
2022-12-24 20:08:38 +0300

    [Frontend] Add sign in page

--- src/SaM.AnyDeals.Web/src/App.tsx
+++ src/SaM.AnyDeals.Web/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Home from "./pages/home/Home";
+import SignIn from "./pages/signin/SignIn";
 import SignUp from "./pages/signup/SignUp";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
       </Routes>
     </div>
   );
---
+++ src/SaM.AnyDeals.Web/src/pages/signin/SignIn.tsx
@@ -0,0 +1,82 @@
+import "./SignIn.scss";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { useForm } from "react-hook-form";
+import Input from "../../components/Input";
+import PrimaryButton from "../../components/PrimaryButton";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import axios from "axios";
+
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Invalid email format")
+    .required("Email is a required field"),
+
+  password: yup.string().required("Password is a required field"),
+});
+
+export default function SignIn() {
+  const navigateToHome = () => (window.location.href = "/");
+  const {
+    register,
+    handleSubmit,
+    formState: { isDirty, isValid, errors },
+  } = useForm({
+    mode: "onBlur",
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+    axios
+      .post("/api/auth/login", data)
+      .then((result) => console.log(result))
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div className="signin">
+      <div className="back-line">
+        <div className="line" onClick={navigateToHome}>
+          <div className="row">
+            <ArrowBackIcon />
+          </div>
+          <p className="row nav-text prevent-select">Back</p>
+        </div>
+      </div>
+      <div className="signin__content">
+        <form
+          noValidate
+          className="signin__form"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="signin__title">Authorization</div>
+
+          <div className="form-field form__email">
+            <Input
+              {...register("email", { required: true })}
+              label="Email"
+              error={!!errors.email}
+              helperText={errors?.email?.message}
+            />
+          </div>
+
+          <div className="form-field form__password">
+            <Input
+              {...register("password", { required: true })}
+              label="Password"
+              type="password"
+              error={!!errors.password}
+              helperText={errors?.password?.message}
+            />
+          </div>
+
+          <PrimaryButton type="submit" disabled={!isDirty || !isValid}>
+            Login
+          </PrimaryButton>
+        </form>
+      </div>
+    </div>
+  );
+}
