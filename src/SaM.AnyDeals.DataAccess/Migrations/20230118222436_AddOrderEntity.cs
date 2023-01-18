a5063865f06a86b006e0fdc6475c13ba186541eb
Gleb <70369777+KrawGleb@users.noreply.github.com>
2023-01-19 02:22:03 +0300

    ⚙️ Fix orders' table migration

---
+++ src/SaM.AnyDeals.DataAccess/Migrations/20230118222436_AddOrderEntity.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SaM.AnyDeals.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrderEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AdvertId = table.Column<int>(type: "int", nullable: false),
+                    CustomerId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ExecutorId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    HasCustomerApproval = table.Column<bool>(type: "bit", nullable: false),
+                    HasExecutorApproval = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orders_Adverts_AdvertId",
+                        column: x => x.AdvertId,
+                        principalTable: "Adverts",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Orders_AspNetUsers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Orders_AspNetUsers_ExecutorId",
+                        column: x => x.ExecutorId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "A3BF16BB-378C-4350-8BFF-FF1ED9CB2915",
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "f8f7f881-1a3d-46d6-a74e-17bcd658cc46", "30114e5f-fa10-44df-81a8-9a250136a760" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_AdvertId",
+                table: "Orders",
+                column: "AdvertId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_CustomerId",
+                table: "Orders",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_ExecutorId",
+                table: "Orders",
+                column: "ExecutorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "A3BF16BB-378C-4350-8BFF-FF1ED9CB2915",
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "5e81cb42-e1d8-48f6-8519-f5c2f316872f", "d5029aef-8ae5-4f6c-8ee7-2d4f793cf3b8" });
+        }
+    }
+}
