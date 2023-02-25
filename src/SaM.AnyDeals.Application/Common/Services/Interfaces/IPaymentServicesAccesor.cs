8d036331f82e64c2d5ad482cf71437aea169cdd8
Gleb <70369777+KrawGleb@users.noreply.github.com>
2023-02-25 14:15:08 +0300

    ⚙️ Fix interface

--- src/SaM.AnyDeals.Application/Common/Services/Interfaces/IPaymentServicesAccesor.cs
+++ src/SaM.AnyDeals.Application/Common/Services/Interfaces/IPaymentServicesAccesor.cs
@@ -5,5 +5,4 @@ namespace SaM.AnyDeals.Application.Common.Services.Interfaces;
 public interface IPaymentServicesAccesor
 {
     PaymentIntentService PaymentIntentService { get; }
-    RefundService RefundService { get; }
 }
