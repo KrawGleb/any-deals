7d930b76554b026d7e9b8a10596bd17ee13b74e2
Gleb <70369777+KrawGleb@users.noreply.github.com>
2023-03-03 23:00:32 +0300

    ⚙️ Remove cities' population script

--- src/SaM.AnyDeals.DataAccess/Population/Populator.cs
+++ src/SaM.AnyDeals.DataAccess/Population/Populator.cs
@@ -10,8 +10,6 @@ public static class Populator
 {
     public static void Populate(this ModelBuilder modelBuilder)
     {
-        var populatedCountries = modelBuilder.Entity<CountryDbEntry>().Populate();
-        modelBuilder.Entity<CityDbEntry>().Populate(populatedCountries);
         modelBuilder.Entity<CategoryDbEntry>().Populate();
 
         modelBuilder.Entity<IdentityRole>().Populate();
