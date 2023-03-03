9f9c8ded58b719d86253dbe2d8df045f16504537
Gleb <70369777+KrawGleb@users.noreply.github.com>
2023-03-04 02:33:42 +0300

    ⚙️ Merge migrations and optimise population

---
+++ src/SaM.AnyDeals.DataAccess/Migrations/20230303214554_LoadCityAndCountryData.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SaM.AnyDeals.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class LoadCityAndCountryData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                """
+                DROP PROCEDURE IF EXISTS [dbo].[LoadDataFromCSV]
+                """);
+            
+            migrationBuilder.Sql(
+                """
+                CREATE PROCEDURE [dbo].[LoadDataFromCSV]
+                    @csvFilePath NVARCHAR(1000),
+                    @tableName NVARCHAR(1000)
+                AS
+                BEGIN
+                    DECLARE @sql NVARCHAR(MAX)
+                    SET @sql = ' TRUNCATE TABLE ' + @tableName +
+                               ' BULK INSERT ' + @tableName +
+                               ' FROM ''' + @csvFilePath + ''' ' +
+                               ' WITH ( ' +
+                               ' FIELDTERMINATOR = '','', ' +
+                               ' ROWTERMINATOR = ''\n'', ' +
+                               ' KEEPIDENTITY)'
+
+                    EXEC sp_executesql @sql
+                END
+                """);
+
+            var countriesFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Migrations/Data/countries.csv");
+            var citiesFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Migrations/Data/cities.csv");
+
+            migrationBuilder.Sql(
+                $"""
+                    EXEC [dbo].[LoadDataFromCSV] '{countriesFile}', 'Countries'
+                """);
+            migrationBuilder.Sql(
+                $"""
+                    EXEC [dbo].[LoadDataFromCSV] '{citiesFile}', 'Cities'
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
